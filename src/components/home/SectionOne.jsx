e10fdd642f415af83a2d2857ef8289bd42e68a52
Azathot11 <azathotkamdoum@gmail.com>
2023-09-13 12:35:19 +0100

    home page complete

--- src/components/home/SectionOne.jsx
+++ src/components/home/SectionOne.jsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 
 const SectionOne = () => {
     return (
-        <div className={`  relative  h-full  md:min-h-screen md:h-full w-full    bg-transparent pt-40 pb-20 gap-y-10 sm:gap-y-0  flex flex-col items-center  ${styles.paddingX} `}>
+        <div className={`  relative  h-full  md:min-h-screen md:h-full w-full    bg-transparent pt-40 gap-y-10 sm:gap-y-0  flex flex-col items-center  ${styles.paddingX} `}>
             <div className={'flex flex-col items-center gap-y-5   z-10 xs:absolute '}>
                 <h1 className={'text-center text-3xl sm:text-5xl md:text-5xl  lg:text-6xl font-space font-bold leading-[50px] sm:leading-[60px] md:leading-[60px] lg:leading-[80px] tracking-[-0.5px]'}>
                     Up your <span className={'text-[#7F56D9]'}>Skills</span> <br/>  to Advance Your  <span className={'text-[#7F56D9]'}>Career</span> <br className={'hidden sm:flex'}/> Path
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { ContextProvider } from './context/contextProvider';
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ContextProvider>
     <BrowserRouter>
+        <ScrollToTop/>
       <App />
     </BrowserRouter>
     </ContextProvider>
