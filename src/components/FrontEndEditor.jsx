85a85738321e63f4cd93127764edc575c9973d6e
ATO <tonybradpit@gmail.com>
2023-09-18 04:04:03 -0700

    added code editor

--- src/components/FrontEndEditor.jsx
+++ src/components/FrontEndEditor.jsx
@@ -1,70 +1,107 @@
-import React, { useState, useEffect } from 'react';
-import AceEditor from 'react-ace';
-import 'ace-builds/src-noconflict/mode-html';
-import 'ace-builds/src-noconflict/mode-javascript';
-import 'ace-builds/src-noconflict/mode-css';
-import 'ace-builds/src-noconflict/theme-monokai';
+import React from 'react'
+import { FaHtml5,FaChevronDown, FaCss3, FaJs } from 'react-icons/fa'
+import { FcSettings } from 'react-icons/fc'
+import SplitPane from 'react-split-pane'
 
 const FrontEndEditor = () => {
-  const [htmlCode, setHtmlCode] = useState('');
-  const [cssCode, setCssCode] = useState('');
-  const [jsCode, setJsCode] = useState('');
-  const [srcDoc, setSrcDoc] = useState('');
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setSrcDoc(`
-        <html>
-          <body>${htmlCode}</body>
-          <style>${cssCode}</style>
-          <script>${jsCode}</script>
-        </html>
-      `);
-    }, 250);
-
-    return () => clearTimeout(timeout);
-  }, [htmlCode, cssCode, jsCode]);
-
   return (
-    <div className="p-0 m-0">
-      <h1 className="text-2xl font-bold mb-5">Live Code Editor</h1>
-      <div className="flex flex-col h-full md:h-[600px]">
-        <div className="flex-grow grid md:grid-cols-3 gap-4">
-          <AceEditor
-            mode="html"
-            theme="monokai"
-            value={htmlCode}
-            onChange={setHtmlCode}
-            name="HTML_EDITOR"
-            editorProps={{ $blockScrolling: true }}
-          />
-          <AceEditor
-            mode="css"
-            theme="monokai"
-            value={cssCode}
-            onChange={setCssCode}
-            name="CSS_EDITOR"
-            editorProps={{ $blockScrolling: true }}
-          />
-          <AceEditor
-            mode="javascript"
-            theme="monokai"
-            value={jsCode}
-            onChange={setJsCode}
-            name="JS_EDITOR"
-            editorProps={{ $blockScrolling: true }}
-          />
-        </div>
-        <iframe
-          title="output"
-          className="w-full h-full mt-4 bg-white"
-          sandbox="allow-scripts"
-          frameBorder="0"
-          srcDoc={srcDoc}
-        />
-      </div>
-    </div>
-  );
-};
+    <>
+      <div className='w-screen h-screen flex flex-col items-start justify-start overflow-hidden'>
+        { /* alert section */}
 
-export default FrontEndEditor;
+
+        { /* header section */}
+
+
+        {/* coding section */}
+
+        <div>
+          {/* horrizontal */}
+          <SplitPane split='horizontal' minSize={100} maxSize={-100} defaultSize={"50%"}>
+
+          {/* top coding section */}
+          <SplitPane split='vertical' minSize={500}>
+            {/**html code */}
+            <div className='w-full h-full flex flex-col items-start justify-start'>
+
+
+              <div className='w-full flex items-center justify-between'>
+
+                <div className='bg-gray-600 px-4 y-2 border-t-4 flex item-center justify-center gap-3 border-t-gray-500'>
+                  <FaHtml5 className='text-xl text-red-500'/>
+                  <p className='text-gray-300 font-semibold '>HTML</p>
+                </div>
+
+                {/**icons */}
+
+                <div className='cursor-pointer flex items-center justify-center gap-4 px-4'>
+                  <FcSettings className='text-xl'/>
+                  <FaChevronDown className='text-xl text-gray-400'/>
+                </div>
+              </div>
+              <div>code mirror</div>
+
+            </div>
+
+            <SplitPane split='vertical' minSize={500}>
+
+              {/**css code */}
+                <div className='w-full h-full flex flex-col items-start justify-start'>
+
+
+              <div className='w-full flex items-center justify-between'>
+
+                <div className='bg-gray-600 px-4 y-2 border-t-4 flex item-center justify-center gap-3 border-t-gray-500'>
+                  <FaCss3 className='text-xl text-sky-500'/>
+                  <p className='text-gray-300 font-semibold '>CSS</p>
+                </div>
+
+                {/**icons */}
+
+                <div className='cursor-pointer flex items-center justify-center gap-4 px-4'>
+                  <FcSettings className='text-xl'/>
+                  <FaChevronDown className='text-xl text-gray-400'/>
+                </div>
+              </div>
+              <div>code mirror</div>
+
+            </div>
+
+              {/**js code */}
+
+                <div className='w-full h-full flex flex-col items-start justify-start'>
+
+
+              <div className='w-full flex items-center justify-between'>
+
+                <div className='bg-gray-600 px-4 y-2 border-t-4 flex item-center justify-center gap-3 border-t-gray-500'>
+                  <FaJs className='text-xl text-yellow-500'/>
+                  <p className='text-gray-300 font-semibold '>JS</p>
+                </div>
+
+                {/**icons */}
+
+                <div className='cursor-pointer flex items-center justify-center gap-4 px-4'>
+                  <FcSettings className='text-xl'/>
+                  <FaChevronDown className='text-xl text-gray-400'/>
+                </div>
+              </div>
+              <div>code mirror</div>
+
+            </div>
+              
+            </SplitPane>
+          </SplitPane>
+
+          <div></div>
+          {/* bottom coding section */}
+          </SplitPane>
+
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default FrontEndEditor
